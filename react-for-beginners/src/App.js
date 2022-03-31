dbc49008d39144a0a510c9158298edb34b8b0a52
Hajin Kim <rhrnak624@naver.com>
2022-03-31 14:37:36 +0900

    useEffect - cleanup function

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,34 +1,27 @@
 import {useState, useEffect} from 'react';
 
-// state가 변하면 모든 코드가 또 실행됨.
+function Hello() {
+  // Component가 새로 생성될때만 실행됨.
+  // 첫번째 매개변수가 반환하는 함수는 소멸할 때 실행됨.
+  useEffect(() => {
+    console.log("hi :)");
+    return () => {
+      console.log("bye :(")
+    }
+  }, []);
+  return <h1>Hello</h1>;
+}
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev+1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  // useEffect() : function이 렌더링에 상관없이 한번만 실행되도록 함.
-  useEffect(() => console.log("i run only once"), []);
-
-  // 두 번째 매개변수에 넣은 값이 변할 때마다 실행
-  useEffect(() => {
-    console.log("Search For ", keyword);
-  }, [keyword]);
-
-  useEffect(() => {
-    console.log("i run when 'counter' changes");
-  }, [counter])
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(prev => !prev)
   return (
     <div>
-      <input 
-        value={keyword} 
-        onChange={onChange} 
-        type="text" 
-        placeholder="Search here..." />
-       <h1>{counter}</h1>
-       <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>
+        {showing ? "Hide" : "Show"}
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
